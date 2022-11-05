67fed519442081461f26b5bc8f1d06adaf5bbd58
codedlibra <mrcoded98@gmail.com>
2022-11-05 02:38:09 -0700

    refactor: redesigned the profile page to suit requirement

---
+++ src/components/Profile.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import profile__img from "./img/profile__img.png";
+import github from "./img/github.png";
+import slack from "./img/slack.png";
+import Icon from "./img/Icon.png";
+import Icon_new from "./img/Icon_new.png";
+
+function Profile() {
+  return (
+    <>
+    <div className="container-fluid">
+      <div>
+        <img src={Icon}
+           className="share_icon"
+              alt="share_icon_webview"/>
+
+        <img src={Icon_new}
+           className="share_icon2"
+                alt="share_icon_mobileview"/>
+    </div>
+
+      <div className="flex justify-center pt-8">
+
+        <img id="profile__img"
+         className="items-center"
+          src={profile__img}
+           alt="profile_image"/>
+
+      </div>
+
+      <h2 id="twitter">CodedLibra</h2>
+
+      <h2 id="slack" className="mt-0">CodedLibra</h2>
+
+      <div className="container">
+      <a href="http://twitter.com/CodedLibra"
+       className="btn btn-block btn-link mb-3"
+       >Twitter Link</a>
+
+      <a id="btn__zuri"
+       href="https://training.zuri.team/"
+        title="Want to Learn Coding/Design Better?"
+        className="btn btn-block btn-link mb-3"
+         >Zuri Team</a>
+
+      <a id="books"
+       href="http://books.zuri.team"
+        className="btn btn-block btn-link mb-3"
+        >Zuri Books</a>
+
+      <a id="book__python"
+       href="https://books.zuri.team/python-for-beginners?ref_id=adeola-ogundijo"
+        className="btn btn-block btn-link mb-3"
+        >Python Books</a>
+
+      <a id="pitch"
+       href="https://background.zuri.team"
+        title="Be sure not to Employ a Bad Developer"
+         className="btn btn-block btn-link mb-3"
+         >Background Check for Coders</a>
+
+      <a id="book__design"
+       href="https://books.zuri.team/design-rules"
+        title="Take a look at the Free Design by Zuri!>"
+         className="btn btn-block btn-link mb-3"
+         >Design Books</a>
+
+      <Link id="contact"
+       to="/contact"
+        className="btn btn-block btn-link mb-4"
+         >Contact Me</Link>
+    </div>
+
+    <div className="profile_bottom">
+        <img src={slack} className="mr-3" alt="slack_icon"/>
+        <img src={github} className="ml-3"alt="github_icon"/>
+    </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Profile
