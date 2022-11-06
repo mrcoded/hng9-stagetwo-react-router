62646a985eadd4b021cffd7cdecf27676f7f61f0
codedlibra <mrcoded98@gmail.com>
2022-11-05 17:38:12 -0700

    refactor: used flex styling in place of grid columns and rows in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,21 +8,10 @@ function Footer() {
     <div className="footerLine"></div>
     <div className="container-fluid mt-3 pb-5">
           <div className="footerContent">
-          <div className="row">
-            <div className="col-xl-0"></div>
-            <div className="col-sm-4 col-lg-4 col-xl-5 mt-0">
-                <img src={Vector} alt="zuri_icon"/>
-            </div>
-            {/* <div className="col-lg col-xl-1"></div> */}
-            <div className="col-sm-5 col-lg-6 col-xl-6 mt-2">
+            <div className="d-flex justify-content-between flex-wrap">
+              <img src={Vector} alt="zuri_icon"/>
               <footer className="h6"> HNG Internship 9 Frontend Task</footer>
-            </div>
-
-            {/* <div className="col-lg-1 col-xl-3"></div> */}
-             <div className="col-sm-3 col-lg-2 col-xl-1 mt-2">
-              <img src={I4G} alt="ingressive icon"/> 
-            </div>
-
+              <img src={I4G} alt="ingressive icon"/>
             </div>
           </div>
         </div>
